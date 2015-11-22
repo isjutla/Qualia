724f1f9f27e7054dbd6f65b8c7633ff4c97e786e
isjutla <isjutla@berkeley.edu>
2015-11-22 18:56:35 -0500

    cleaning up format and imports

--- Alphabet-Sort/src/main/java/Solution.java
+++ Alphabet-Sort/src/main/java/Solution.java
@@ -1,8 +1,7 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Runtime: O(k*n) where k is the maximum size of a word
@@ -12,34 +11,33 @@ import java.util.regex.*;
  * (List.get, List.add, and HashMap.get are all O(1) and are done
  * for each element of the list). bucketToList also process at most
  * n values. Thus the total runtime is O(k*n)
- *
+ * <p/>
  * Author: Inderjit Jutla
  * Date: 2015-11-22
  */
 public class Solution {
 
     /**
-     *
      * @param words is the list of words to sort in order
      * @param order describes the lexographic ordering required
      * @return the ordered version of words
      */
     public static List<String> order(List<String> words, String order) {
         Map<Character, Integer> radix = new HashMap<Character, Integer>();
-        for(int i = 0; i < order.length(); i++) {
+        for (int i = 0; i < order.length(); i++) {
             radix.put(order.charAt(i), i + 1);
         }
         int pos = 0;
-        for(String word: words) {
-            if(word.length() - 1 > pos) {
+        for (String word : words) {
+            if (word.length() - 1 > pos) {
                 pos = word.length() - 1;
             }
         }
-        while(pos >= 0) {
+        while (pos >= 0) {
             List<List<String>> buck = new ArrayList<List<String>>();
             initBuck(buck, radix.size() + 1);
-            for(String a: words){
-                if(a.length() >= pos+1) {
+            for (String a : words) {
+                if (a.length() >= pos + 1) {
                     int r = radix.get(a.charAt(pos));
                     List<String> rad = buck.get(r);
                     rad.add(a);
@@ -51,20 +49,20 @@ public class Solution {
             pos = pos - 1;
             words = bucketToList(buck);
         }
-        return(words);
+        return (words);
 
     }
 
     public static void initBuck(List<List<String>> buck, int size) {
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             buck.add(new ArrayList<String>());
         }
     }
 
     public static List<String> bucketToList(List<List<String>> buck) {
         List<String> test = new ArrayList<String>();
-        for(List<String> bucket: buck) {
-            for(String val: bucket) {
+        for (List<String> bucket : buck) {
+            for (String val : bucket) {
                 test.add(val);
             }
         }
--- Alphabet-Sort/src/test/java/SolutionTest.java
+++ Alphabet-Sort/src/test/java/SolutionTest.java
@@ -1,11 +1,9 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 
 
